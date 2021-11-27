55a17b50edd651c67129f3d1aa4a53531ca17de5
zhangdengyuan <zhangdengyuan@17paipai.cn>
2021-11-27 14:03:18 +0800

    init

---
+++ app.go
@@ -0,0 +1,12 @@
+//@Desc : \\todo
+//@Author : zhangdengyuan 2021/11/27 11:38 上午
+//@Update: xxx 2021/11/27 11:38 上午
+
+package gardner
+
+
+type ServiceI interface {
+	Name() string
+	OnInit()
+
+}
---
+++ logger/interface.go
@@ -0,0 +1,9 @@
+//@Desc : \\todo
+//@Author : zhangdengyuan 2021/11/27 12:01 下午
+//@Update: xxx 2021/11/27 12:01 下午
+
+package logger
+
+type LogI interface{
+
+}
